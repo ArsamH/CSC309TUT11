0eb614ada654a30c6f405d6df92aaf5430212128
Arsam Hassan <arsam.hassan@cattleytics.com>
2025-10-05 16:23:27 -0400

    f

--- T5/index.js
+++ T5/index.js
@@ -78,14 +78,16 @@ app.get("/notes/:noteId", (req, res) => {
 
 app.patch("/notes/:noteId", (req, res) => {
   const noteId = Number(req.params.noteId);
-  if (done !== "true" && done !== "false") {
+  if (isNaN(noteId)) {
     return res.status(400).send("Bad request");
   }
-
   if (noteId < 0 || noteId >= data.length) {
     return res.status(404).send("Not found");
   }
   const done = req.query.done;
+  if (done !== "true" && done !== "false") {
+    return res.status(400).send("Bad request");
+  }
   const completed = done === "true";
   data[noteId] = { ...data[noteId], completed: completed };
   return res.status(200).json(data[noteId]);
