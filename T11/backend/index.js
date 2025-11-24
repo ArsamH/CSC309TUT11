d0b71df763c6c3145132fe8ebf6ef4c38c96cd45
Arsam Hassan <arsam.hassan@cattleytics.com>
2025-11-23 21:26:12 -0500

    f

--- T11/backend/index.js
+++ T11/backend/index.js
@@ -1,11 +1,24 @@
 import express from "express";
 import routes from "./routes.js";
+import cors from "cors";
+import dotnev from "dotenv";
 // TODO: complete me (loading the necessary packages)
+
+dotnev.config();
+
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 const app = express();
 
 // TODO: complete me (CORS)
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
-app.use('', routes);
+app.use("", routes);
 
 export default app;
--- T11/frontend/src/contexts/AuthContext.jsx
+++ T11/frontend/src/contexts/AuthContext.jsx
@@ -1,12 +1,14 @@
-import React, { createContext, useContext, useEffect, useState} from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext(null);
 
 // TODO: get the BACKEND_URL.
 
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+
 /*
- * This provider should export a `user` context state that is 
+ * This provider should export a `user` context state that is
  * set (to non-null) when:
  *     1. a hard reload happens while a user is logged in.
  *     2. the user just logged in.
@@ -15,56 +17,122 @@ const AuthContext = createContext(null);
  *     2. the user just logged out.
  */
 export const AuthProvider = ({ children }) => {
-    const navigate = useNavigate();
-    // const user = null; // TODO: Modify me.
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
 
-    useEffect({
-        // TODO: complete me, by retriving token from localStorage and make an api call to GET /user/me.
-    }, [])
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setUser(null);
+      return;
+    }
+    (async () => {
+      try {
+        const res = await fetch(`${BACKEND_URL}/user/me`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
-    /*
-     * Logout the currently authenticated user.
-     *
-     * @remarks This function will always navigate to "/".
-     */
-    const logout = () => {
-        // TODO: complete me
+        if (!res.ok) {
+          setUser(null);
+          localStorage.removeItem("token");
+          return;
+        }
 
-        navigate("/");
-    };
+        const data = await res.json();
+        setUser(data.user);
+      } catch (err) {
+        setUser(null);
+      }
+    })();
+  }, []);
 
-    /**
-     * Login a user with their credentials.
-     *
-     * @remarks Upon success, navigates to "/profile". 
-     * @param {string} username - The username of the user.
-     * @param {string} password - The password of the user.
-     * @returns {string} - Upon failure, Returns an error message.
-     */
-    const login = async (username, password) => {
-        // TODO: complete me
-        return "TODO: complete me";
-    };
+  /*
+   * Logout the currently authenticated user.
+   *
+   * @remarks This function will always navigate to "/".
+   */
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+    navigate("/");
+  };
 
-    /**
-     * Registers a new user. 
-     * 
-     * @remarks Upon success, navigates to "/".
-     * @param {Object} userData - The data of the user to register.
-     * @returns {string} - Upon failure, returns an error message.
-     */
-    const register = async (userData) => {
-        // TODO: complete me
-        return "TODO: complete me";
-    };
+  /**
+   * Login a user with their credentials.
+   *
+   * @remarks Upon success, navigates to "/profile".
+   * @param {string} username - The username of the user.
+   * @param {string} password - The password of the user.
+   * @returns {string} - Upon failure, Returns an error message.
+   */
+  const login = async (username, password) => {
+    try {
+      const res = await fetch(`${BACKEND_URL}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
-    return (
-        <AuthContext.Provider value={{ user, login, logout, register }}>
-            {children}
-        </AuthContext.Provider>
-    );
+      if (!res.ok) {
+        const err = await res.json();
+        return err.message;
+      }
+
+      const data = await res.json();
+
+      localStorage.setItem("token", data.token);
+      const userResponse = await fetch(`${BACKEND_URL}/user/me`, {
+        headers: { Authorization: `Bearer ${data.token}` },
+      });
+
+      const userData = await userResponse.json();
+      setUser(userData.user);
+
+      navigate("/profile");
+    } catch (err) {
+      return "login error";
+    }
+  };
+
+  /**
+   * Registers a new user.
+   *
+   * @remarks Upon success, navigates to "/".
+   * @param {Object} userData - The data of the user to register.
+   * @returns {string} - Upon failure, returns an error message.
+   */
+  const register = async (userData) => {
+    try {
+      const res = await fetch(`${BACKEND_URL}/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+
+      if (!res.ok) {
+        const err = await res.json();
+        return err.message;
+      }
+      navigate("/success");
+    } catch (err) {
+      return "register error";
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout, register }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 };
